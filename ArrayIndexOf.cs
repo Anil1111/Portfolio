8a94b68c2665f9de6f9c1b89b5d96620abbba8ea
jhong3 <48836291+jhong3@users.noreply.github.com>
2019-04-15 18:48:54 -0700

    Update ArrayIndexOf.cs

--- ArrayIndexOf.cs
+++ ArrayIndexOf.cs
@@ -1,12 +1,18 @@
-﻿/*
+/*
    Source: https://www.dotnetperls.com/array
    Author: Sam Allen
    Summary: This program demonstrates IndexOf method.
    Date: 13 Apr 2019
-   Modifications: ----
+   Modifications: Added user's input for custom array
+                  list.
    Example Output:
-                    1
-                   -1
+                   Enter 3 animals:
+                   Dog
+                   Cat
+                   Bat
+                   Which index would you like to see?
+                   Cat
+                   1
 */
 
 using System;
@@ -15,15 +21,28 @@ class Program
 {
     static void Main()
     {
-        string[] array = { "cat", "dog", "bird", "fish" };
+        // The program now strings inputs from the user.
+        // Instead of rewriting Console.ReadLine() three
+        // times, the for loop can be used to store and
+        // read the strings three times in one line.
+        string[] animals = new string[3];
+        Console.WriteLine("Enter 3 animals:");
+        for (int i = 0; i < animals.Length; i++)
+        {
+            animals[i] = Console.ReadLine();
+        }
 
-        // The dog string is at index 1.
-        int dogIndex = Array.IndexOf(array, "dog");
-        Console.WriteLine(dogIndex);
+        // With the animals index stored with user's input,
+        // the program requests which index the user would
+        // like to see (the numeric position). By typing
+        // 'Cat', the program will return 1 since the Cat
+        // is in index 1 of the array.
+        Console.WriteLine("Which index would you like to access?");
+        string input = Console.ReadLine();
+        int inputIndex = Array.IndexOf(animals, input);
+        Console.WriteLine(inputIndex);
 
-        // There is no monkey string in the array.
-        // ... So IndexOf returns -1.
-        int monkeyIndex = Array.IndexOf(array, "monkey");
-        Console.WriteLine(monkeyIndex);
+        Console.ReadLine();
+
     }
 }
