73c3435f2611a7c44275b215f5dbf589ed8d9eea
jhong3 <48836291+jhong3@users.noreply.github.com>
2019-04-15 18:56:19 -0700

    Add files via upload

---
+++ ConstructorExample.cs
@@ -0,0 +1,40 @@
+﻿/*
+   Source: https://www.dotnetperls.com/constructor
+   Author: Sam Allen
+   Summary: This program demonstrates how to use the Base
+            keyword to allow the derived constructor to invoke
+            the base class constructor.
+   Date: 13 Apr 2019
+   Modifications: ----
+   Example Output:
+                    Bird() called with 450
+                    Parrot called with 450
+                    :::DONE:::
+ */
+
+using System;
+
+public class Bird
+{
+    public Bird(int value)
+    {
+        Console.WriteLine($"Bird() called with {value}");
+    }
+}
+
+public class Parrot : Bird
+{
+    public Parrot(int value) : base(value)
+    {
+        Console.WriteLine($"Parrot called with {value}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Parrot parrot = new Parrot(450);
+        Console.WriteLine(":::DONE:::");
+    }
+}
